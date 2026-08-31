c883e70ded00f1d20824c7356c203bc5409eec14
Parth <parthhkdigiverse@gmail.com>
2026-08-31 15:52:17 +0530

    chore: remove .vercel build output from git and add to .gitignore

--- .vercel/output/functions/__server.func/_ssr/recycle-bin-D9eFl4Xc.mjs
+++
@@ -1,5 +0,0 @@
-import { u as RecycleBin } from "./RecycleBin-DO8goFRy.mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/recycle-bin-D9eFl4Xc.js
-var SplitComponent = RecycleBin;
-//#endregion
-export { SplitComponent as component };
--- .vercel/output/static/assets/recycle-bin-BpxOo5E9.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./RecycleBin-CYr0AmLn.js";var t=e;export{t as component};
